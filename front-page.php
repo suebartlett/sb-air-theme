384c891d8f2bae14ab82520e71aaea3a377e2ec1
Rik Deakin <you@example.com>
2018-06-09 22:10:16 +0100

    Fixed missing wploop in front-page.php?

--- front-page.php
+++ front-page.php
@@ -9,7 +9,6 @@
  *
  * @package air-light
  */
-
 // Featured image.
 $featured_image = '';
 if ( has_post_thumbnail() ) :
@@ -17,23 +16,31 @@ if ( has_post_thumbnail() ) :
 else :
 	$featured_image = get_theme_file_uri( 'images/default.jpg' );
 endif;
-
-get_header(); ?>
-
-<header class="entry-header-demo">
-
-</header>
+get_header();
+?>
 
 <div id="content" class="content-area">
-  <main id="main" class="site-main">
-		<?php if ( have_posts() ) {
-						while ( have_posts() ) {
-							the_post();
-							the_content();
-						}
-					} else {
-						get_template_part( 'template-parts/content', 'none' );
-					}  ?>
+  <main role="main" id="main" class="site-main">
+
+    <div class="entry-header-demo">
+      <h1><span class="accent"><?php echo esc_html_e( 'air-light ', 'air-light' ); echo esc_attr( AIR_LIGHT_VERSION, 'air-light' ); ?></span><?php echo esc_html_e( 'a WordPress starter theme', 'air-light' ); ?></h1>
+    </div>
+
+    <div class="block">
+      <div class="container">
+
+        <?php if ( have_posts() ) {
+        	while ( have_posts() ) {
+	      		the_post();
+	      		the_content();
+					}
+        } else {
+        	get_template_part( 'template-parts/content', 'none' );
+        }  ?>
+
+      </div>
+    </div>
+
   </main><!-- #main -->
 </div><!-- #primary -->
 
