d4c9ca9d5cb2e5947876ace0f42316098688bd2d
Rik Deakin <you@example.com>
2018-08-29 21:28:31 +0100

    added pinterest meta tag to header.php

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
+  <meta name="p:domain_verify" content="94c8577eaf287e9ad5d8fc175604701b"/>
 
   <?php wp_head(); ?>
 </head>
